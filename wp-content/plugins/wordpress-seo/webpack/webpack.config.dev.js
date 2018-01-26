0c448fdd9f5c2f236504a2eff7a255d699fa8f61
“Newton <ncdfilho@gmail.com>
2018-01-26 13:21:25 +0000

    remove all

--- wp-content/plugins/wordpress-seo/webpack/webpack.config.dev.js
+++
@@ -1,8 +0,0 @@
-/* global require, module */
-const defaults = require( "./webpack.config.default" ).defaults;
-
-const devConfig ={
-	devtool: "eval",
-};
-
-module.exports = defaults( devConfig );
